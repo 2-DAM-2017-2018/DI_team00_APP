c615e60743bedd0cef1505211406ec491bc54d43
Daniel Muñoz <daniloja17@gmail.com>
2017-11-10 08:40:49 +0100

    Creacion de clase OverView y overViewController 1.1

--- src/app_reserva_view/ClaseOverViewController.java
+++ src/app_reserva_view/ClaseOverViewController.java
@@ -3,16 +3,11 @@ package app_reserva_view;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 
-public class ClaseOverViewController {
+public class ClaseOverViewController 
+{
     
-     @FXML
-    private TableView<Clases> clasesTable;
-    @FXML
-    private TableColumn<Clases, String> clasesColumn;
-    
+   
     @FXML
     private Button reservaButton;
     
